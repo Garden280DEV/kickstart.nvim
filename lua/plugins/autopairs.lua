ffec5d3d0c012a9c841691fade9082d028f57d25
GardinusDEV <alekseyshilovskiy@yandex.ru>
2026-03-28 21:14:49 +0300

    autotag + sylua change

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,10 +1,18 @@
--- autopairs
--- https://github.com/windwp/nvim-autopairs
-
----@module 'lazy'
----@type LazySpec
 return {
-  'windwp/nvim-autopairs',
-  event = 'InsertEnter',
-  opts = {},
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = {},
+  },
+
+  {
+    'windwp/nvim-ts-autotag',
+    opts = {
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = true,
+      },
+    },
+  },
 }
