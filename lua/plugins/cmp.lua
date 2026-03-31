3d084f7579ba9872dc04be8af74902380b9b9b77
GardinusDEV <alekseyshilovskiy@yandex.ru>
2026-03-31 13:30:42 +0300

    cmp format

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,7 +3,6 @@ return { -- Autocompletion
     event = 'VimEnter',
     version = '1.*',
     dependencies = {
-        -- Snippet Engine
         {
             'L3MON4D3/LuaSnip',
             version = '2.*',
@@ -15,35 +14,20 @@ return { -- Autocompletion
                 return 'make install_jsregexp'
             end)(),
             dependencies = {
-                -- `friendly-snippets` contains a variety of premade snippets.
-                --    See the README about individual language/framework/plugin snippets:
-                --    https://github.com/rafamadriz/friendly-snippets
-                -- {
-                --   'rafamadriz/friendly-snippets',
-                --   config = function()
-                --     require('luasnip.loaders.from_vscode').lazy_load()
-                --   end,
-                -- },
+                {
+                    'rafamadriz/friendly-snippets',
+                    config = function() require('luasnip.loaders.from_vscode').lazy_load() end,
+                },
             },
             opts = {},
         },
     },
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
     opts = {
         keymap = {
             -- 'default' (recommended) for mappings similar to built-in completions
-            --   <c-y> to accept ([y]es) the completion.
-            --    This will auto-import if your LSP supports it.
-            --    This will expand snippets if the LSP sent a snippet.
             -- 'super-tab' for tab to accept
             -- 'enter' for enter to accept
             -- 'none' for no mappings
-            --
-            -- For an understanding of why the 'default' preset is recommended,
-            -- you will need to read `:help ins-completion`
-            --
-            -- No, but seriously. Please read `:help ins-completion`, it is really good!
             --
             -- All presets have the following mappings:
             -- <tab>/<s-tab>: move to right/left of your snippet expansion
