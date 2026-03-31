275d9b04ee616d890cf1a1f68cc250a7f6010031
GardinusDEV <alekseyshilovskiy@yandex.ru>
2026-03-31 13:34:00 +0300

    godot format

--- lua/plugins/godot.lua
+++ lua/plugins/godot.lua
@@ -1,5 +1,10 @@
 return {
     'Mathijs-Bakker/godotdev.nvim',
     init = function() vim.lsp.enable { 'gdscript' } end,
-    dependencies = { 'nvim-lspconfig', 'nvim-dap', 'nvim-dap-ui', 'nvim-treesitter' },
+    dependencies = {
+        'nvim-lspconfig',
+        'nvim-dap',
+        'nvim-dap-ui',
+        'nvim-treesitter',
+    },
 }
