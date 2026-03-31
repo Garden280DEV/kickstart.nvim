18e62b5e750daa3f60912c2f1ec41e0346398cab
GardinusDEV <alekseyshilovskiy@yandex.ru>
2026-03-31 03:20:55 +0300

    Revert "format indent_line" wtf
    This reverts commit 943d4e0407d7a7377cca96ada4204770e5c2aaf7.

--- lua/plugins/indent_line.lua
+++ lua/plugins/indent_line.lua
@@ -1,4 +1,13 @@
+-- Add indentation guides even on blank lines
+
+---@module 'lazy'
+---@type LazySpec
 return {
-    'lukas-reineke/indent-blankline.nvim',
-    opts = {},
+  'lukas-reineke/indent-blankline.nvim',
+  -- Enable `lukas-reineke/indent-blankline.nvim`
+  -- See `:help ibl`
+  main = 'ibl',
+  ---@module 'ibl'
+  ---@type ibl.config
+  opts = {},
 }
