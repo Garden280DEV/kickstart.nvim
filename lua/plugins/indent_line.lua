943d4e0407d7a7377cca96ada4204770e5c2aaf7
GardinusDEV <alekseyshilovskiy@yandex.ru>
2026-03-31 03:19:19 +0300

    format indent_line

--- lua/plugins/indent_line.lua
+++ lua/plugins/indent_line.lua
@@ -1,13 +1,4 @@
--- Add indentation guides even on blank lines
-
----@module 'lazy'
----@type LazySpec
 return {
-  'lukas-reineke/indent-blankline.nvim',
-  -- Enable `lukas-reineke/indent-blankline.nvim`
-  -- See `:help ibl`
-  main = 'ibl',
-  ---@module 'ibl'
-  ---@type ibl.config
-  opts = {},
+    'lukas-reineke/indent-blankline.nvim',
+    opts = {},
 }
