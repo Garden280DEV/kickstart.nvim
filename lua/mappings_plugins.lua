e8737353c29a74e731993773631f1715db4f521d
GardinusDEV <alekseyshilovskiy@yandex.ru>
2026-04-04 18:22:22 +0300

    telescope mappings

--- lua/mappings_plugins.lua
+++ lua/mappings_plugins.lua
@@ -1,6 +1,7 @@
 local M = {}
 
-local ls = require 'luasnip'
+local luasnip = require 'luasnip'
+local telescope = require 'telescope.builtin'
 
 M.mappings = {
     -- snippets
@@ -8,14 +9,14 @@ M.mappings = {
         'i',
         '<C-l>',
         function()
-            if ls.locally_jumpable(1) then ls.jump(1) end
+            if luasnip.locally_jumpable(1) then luasnip.jump(1) end
         end,
     },
     {
         'i',
         '<C-h>',
         function()
-            if ls.locally_jumpable(-1) then ls.jump(-1) end
+            if luasnip.locally_jumpable(-1) then luasnip.jump(-1) end
         end,
     },
 
@@ -27,6 +28,19 @@ M.mappings = {
     { 'n', '<Leader>ml', '<CMD>Lazy<CR>', 'Open Lazy menu' },
     { 'n', '<Leader>mm', '<CMD>Mason<CR>', 'Open Mason menu' },
     { 'n', '<Leader>mt', '<CMD>Themify<CR>', 'Open Themify menu' },
+
+    -- telescope
+    { 'n', '<leader><leader>', telescope.find_files, '[S]earch [F]iles' },
+    { 'n', '<leader>sr', telescope.resume, '[S]earch [R]esume' },
+    { 'n', '<leader>s.', telescope.oldfiles, '[S]earch Recent Files ("." for repeat},' },
+    { 'n', '<leader>sh', telescope.help_tags, '[S]earch [H]elp' },
+    { 'n', '<leader>sk', telescope.keymaps, '[S]earch [K]eymaps' },
+    { 'n', '<leader>ss', telescope.builtin, '[S]earch [S]elect Telescope' },
+    { 'n', '<leader>sg', telescope.live_grep, '[S]earch by [G]rep' },
+    { 'n', '<leader>sd', telescope.diagnostics, '[S]earch [D]iagnostics' },
+    { 'n', '<leader>sc', telescope.commands, '[S]earch [C]ommands' },
+    { 'n', '<leader>sb', telescope.buffers, '[S]earch existing [B]uffers' },
+    { { 'n', 'v' }, '<leader>sw', telescope.grep_string, '[S]earch current [W]ord' },
 }
 
 return M
